b3232d630452e737cf75a59a521a2cb14533adab
Robert Haas <haasrobert@gmx.net>
2020-04-29 09:43:03 +0200

    Code in Polynomial.Qt/MainWindow.cpp massively simplified. Code in Polynomial.Core/Polynomial.cpp also simplified.

--- Polynomial.Core/Polynomial.cpp
+++ Polynomial.Core/Polynomial.cpp
@@ -47,10 +47,34 @@ namespace Polynomial {
 			Coefficients.push_back(cplx(0.0, 0.0));
 		}
 
+		
+#else
+		void Polynomial::Create(unsigned int _deg) {
+			Degree = _deg;
+			Coefficients.reserve(Degree + 1);
+			Roots.reserve(Degree);
+			if (Degree == 2) {
+				FindRoots = [this]() { PolynomialRoots::MidNightFormula(this); };
+			}
+			if (Degree == 3) {
+				FindRoots = [this]() { PolynomialRoots::CardanoMethod(this); };
+			}
+			if (Degree == 4) {
+				FindRoots = [this]() { PolynomialRoots::FerrariMethod(this); };
+			}
+
+			for (auto j = 0; j < Degree; ++j) {
+				Coefficients.push_back(cplx(0.0, 0.0));
+			}
+			Coefficients.push_back(cplx(0.0, 0.0));
+		}
+
+#endif
+
 		void Polynomial::ValidateRoots() {
 			std::vector<cplx> z;
 			z.reserve(4);
-			for (unsigned int j = 0; j < Roots.size();j++) {
+			for (unsigned int j = 0; j < Roots.size(); j++) {
 				z.push_back(Roots[j]);
 			}
 			z.resize(4);
@@ -86,63 +110,6 @@ namespace Polynomial {
 			int ofs = Degree;
 			ResultError = abs(w[ofs - 1] - E) + abs(w[ofs] - D) + abs(w[1 + ofs] - C) + abs(w[2 + ofs] - B);
 		}
-#else
-		void Polynomial::Create(unsigned int _deg) {
-			Degree = _deg;
-			Coefficients.reserve(Degree + 1);
-			Roots.reserve(Degree);
-			if (Degree == 2) {
-				FindRoots = [this]() { PolynomialRoots::MidNightFormula(this); };
-			}
-			if (Degree == 3) {
-				FindRoots = [this]() { PolynomialRoots::CardanoMethod(this); };
-			}
-			if (Degree == 4) {
-				FindRoots = [this]() { PolynomialRoots::FerrariMethod(this); };
-			}
 
-			for (auto j = 0; j < Degree; ++j) {
-				Coefficients.push_back(cplx(0.0, 0.0));
-			}
-			Coefficients.push_back(cplx(0.0, 0.0));
-		}
-		void Polynomial::ValidateRoots() {
-			std::vector<cplx> z;
-			z.reserve(4);
-			for (auto elem : Roots) {
-				z.emplace_back(elem);
-			}
-			z.resize(4);
-
-			std::vector<cplx> w;
-			w.reserve(8);
-			w.emplace_back(0.0);
-			w.emplace_back(0.0);
-			w.emplace_back(0.0);
-			for (auto elem : Coefficients) {
-				w.emplace_back(elem);
-			}
-			w.resize(8);
-
-			//implement Vieta's theorem
-			auto A{ Coefficients.end() - 1 };
-			auto B{ -(z.at(0) + z.at(1) + z.at(2) + z.at(3)) };
-			auto C{ +(z.at(0) * z.at(1) + z.at(0) * z.at(2) + z.at(0) * z.at(3)
-				+ z.at(1) * z.at(2) + z.at(1) * z.at(3) + z.at(2) * z.at(3)) };
-			auto D{ -(z.at(0) * z.at(1) * z.at(2) +
-				z.at(0) * z.at(1) * z.at(3) +
-				z.at(0) * z.at(2) * z.at(3) +
-				z.at(1) * z.at(2) * z.at(3)) };
-			auto E{ +(z.at(0) * z.at(1) * z.at(2) * z.at(3)) };
-
-			B = cplx(A->real(), A->imag()) * B;
-			C = cplx(A->real(), A->imag()) * C;
-			D = cplx(A->real(), A->imag()) * D;
-			E = cplx(A->real(), A->imag()) * E;
-
-			int ofs{ Degree };
-			ResultError = abs(w.at(ofs - 1) - E) + abs(w.at(ofs) - D) + abs(w.at(1 + ofs) - C) + abs(w.at(2 + ofs) - B);
-		}
-#endif
 	}
 }
